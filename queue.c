faa2b40ab29400d60d74c60a18a26eac4208d421
arnitdo <arnav.nitin.deo@gmail.com>
2022-03-17 21:21:18 +0530

    Refactor final files

--- queue.c
+++ queue.c
@@ -1,17 +1,40 @@
-#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+// Queue size must be a compile-time constant
+#ifndef QUEUE_SIZE
+#define QUEUE_SIZE 64
+#endif
+
+typedef struct
+{
+	int* first;
+	int len;
+} queue;
+
+// Fwd declaration
+
+queue* queue_init();
+void queue_insert(queue*, int);
+int queue_front(queue*);
+void queue_delete(queue*);
+void queue_print(queue*);
+int queue_len(queue*);
+
+// Implementation of prototypes
 
 queue* queue_init(){
 	queue* q_ptr = malloc(sizeof(queue));
-	q_ptr->first = malloc(QUEUE_SIZE * sizeof(TYPE));
+	q_ptr->first = malloc(QUEUE_SIZE * sizeof(int));
 	if (q_ptr->first == NULL){
-		fprintf(stderr, "COULD NOT ALLOCATE QUEUE MEMORY FOR %I64u\n", QUEUE_SIZE * sizeof(TYPE));
+		fprintf(stderr, "COULD NOT ALLOCATE QUEUE MEMORY FOR %I64u\n", QUEUE_SIZE * sizeof(int));
 		exit(1);
 	}
 	q_ptr->len = 0;
 	return q_ptr;
 }
 
-void queue_insert(queue* q, TYPE  obj){
+void queue_insert(queue* q, int  obj){
 	if(q->len == QUEUE_SIZE){
 		fprintf(stderr, "QUEUE FILLED WITH %i OBJECTS FROM %p TO %p! CONSIDER HAVING A LARGER QUEUE!\n", QUEUE_SIZE, q->first, (q->first + QUEUE_SIZE));
 		exit(1);
@@ -20,12 +43,12 @@ void queue_insert(queue* q, TYPE  obj){
 	q->len++;
 }
 
-TYPE queue_front(queue* q){
+int queue_front(queue* q){
 	if(q->len == 0){
-		fprintf(stderr, "QUEUE AT %p IS EMPTY!\n", q->first);
+		fprintf(stderr, "QUEUE IS EMPTY!\n");
 		return 0;
 	}
-	TYPE obj = *(q->first);
+	int obj = *(q->first);
 	for (int i = 0; i < q->len - 1; i++){
 		*(q->first + i) = *(q->first + i + 1);
 	}
@@ -58,4 +81,56 @@ void queue_print(queue* q){
 
 int queue_len(queue* q){
 	return q->len;
+}
+
+// Menu driven in main()
+
+int main(){
+	queue* my_queue = queue_init();
+	int input = 0;
+	printf("Queue Implementation by S043 Arnav Deo\n");
+	do {	
+		printf("\nSelect Queue Operation - \n");
+		printf("\t1 : Insert\n");
+		printf("\t2 : Front\n");
+		printf("\t3 : Length\n");
+		printf("\t4 : Print\n");
+		printf("\n\t0 : Exit\n");
+		printf("\nSelect > ");
+		scanf(" %i", &input);
+		switch (input){
+			case 0:{
+				exit(0);
+			}	
+			case 1:{
+				int obj;
+				printf("Enter number to insert : ");
+				scanf(" %i", &obj);
+				queue_insert(my_queue, obj);
+				break;
+			}	
+			case 2:{
+				if (queue_len(my_queue)){
+					int front = queue_front(my_queue);
+					printf("Front number is : %i\n", front);
+				} else {
+					fprintf(stderr, "QUEUE IS EMPTY!\n");
+				}
+				break;
+			}	
+			case 3:{
+				int len = queue_len(my_queue);
+				printf("Length of queue is : %i\n", len);
+				break;
+			}	
+			case 4:{
+				queue_print(my_queue);
+				break;
+			}
+			default:{
+				break;
+			}					
+		}
+	} while (input != 0);
+	return 0;
 }
